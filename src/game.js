1330031d80e163ec8ca07a55a0a096b0e52246db
Michael Cheng <mcheng.work@gmail.com>
2018-08-18 16:10:16 +0800

    Refactor currentCategory

--- src/game.js
+++ src/game.js
@@ -17,16 +17,10 @@ module.exports = function Game() {
   };
 
   const currentCategory = function() {
-    if (places[currentPlayer] == 0) return "Pop";
-    if (places[currentPlayer] == 4) return "Pop";
-    if (places[currentPlayer] == 8) return "Pop";
-    if (places[currentPlayer] == 1) return "Science";
-    if (places[currentPlayer] == 5) return "Science";
-    if (places[currentPlayer] == 9) return "Science";
-    if (places[currentPlayer] == 2) return "Sports";
-    if (places[currentPlayer] == 6) return "Sports";
-    if (places[currentPlayer] == 10) return "Sports";
-    return "Rock";
+    const allPlaces = ["Pop", "Science", "Sports", "Rock", "Pop", "Science", "Sports", "Rock", "Pop", "Science", "Sports", "Rock"]
+    const currentPlace = places[currentPlayer]
+
+    return allPlaces[currentPlace]
   };
 
   this.createRockQuestion = function(index) {
