728e86a1311bc0472e482b3a83b08cac8b492343
Michael Cheng <mcheng.work@gmail.com>
2018-08-19 11:18:37 +0800

    Added test for Board class

--- src/questions.js
+++ src/questions.js
@@ -1,3 +1,5 @@
+'use strict'
+
 class Questions {
   constructor(categories, numQuestions) {
     this.categories = categories
---
+++ test/board.test.js
@@ -0,0 +1,25 @@
+const Board = require("../src/board");
+
+describe('Board', () => {
+  const categories = ["Pop", "Science", "Sports", "Rock"]
+  const totalPlaces = 12
+  let board
+
+  beforeEach(() => {
+    board = new Board(categories, totalPlaces).prepBoard()
+  })
+
+  it('Prepares board places', () => {
+    expect(board.boardPlaces.length).toEqual(totalPlaces)
+  })
+
+  it('Prepare board categories in sequence', () => {
+    board.totalPlaces = 6
+    board.prepBoard()
+    expect(board.boardPlaces).toEqual(["Pop", "Science", "Sports", "Rock", "Pop", "Science"])
+  })
+
+  it('Returns categoryAtPosition', () => {
+    expect(board.categoryAtPosition(4)).toEqual("Pop")
+  })
+});
